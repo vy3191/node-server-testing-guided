240f2caab6ef91510f23581f5f947d7239a2e226
vy3191 <venkyafhs@gmail.com>
2020-03-22 16:28:23 -0400

    Added cross-env dev dependency

--- index.test.js
+++ index.test.js
@@ -10,8 +10,10 @@ const server= require("./index"); // our server won't actually start due to the 
 test("welcome route", async () => {
    const res = await supertest(server).get("/");  
   expect(res.statusCode).toBe(200);
-  expect(res.body.message).toBe("Welcome to our API");
   expect(res.type).toBe("application/json");
+  expect(res.body.message).toBe("Welcome to our API");
+  expect(res.body.message).toHaveLength(18);
+  expect(res.body.message).toMatch(/welcome/i);
 });
 
 
